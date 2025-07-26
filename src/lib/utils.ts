662aab14ae2101d07c6805d0d290804184e18703
koshoss <se7smo7amed@gmail.com>
2025-07-26 22:28:49 +0300

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { supabase } from "@/supabaseClient" // تأكد إن المسار ده صحيح عندك
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const loginWithDiscord = async () => {
+  await supabase.auth.signInWithOAuth({
+    provider: "discord",
+    options: {
+      redirectTo: "https://micheal-portfolio.netlify.app/oauth-redirect", // ← المسار اللي هترجعله بعد تسجيل الدخول
+    },
+  });
+};
